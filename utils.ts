2360ec013cc684a15c2840230456cac9a12e088d
Nolan Barry <nolan.barry@live.com>
2022-12-15 16:41:12 -0700

    Day 7 Part 1

--- utils.ts
+++ utils.ts
@@ -1,12 +1,19 @@
 import fs from 'fs';
 
-export const readInput = (day: number, trim = true): string[] => fs.readFileSync(`day-${day}/input.txt`, 'utf-8').split('\n').map(line => trim ? line.trim() : line )
+export const readInput = (day: number, trim = true): string[] => fs.readFileSync(`day-${day}/input.txt`, 'utf-8').split('\n').map(line => trim ? line.trim() : line)
 export const sum = (list: number[]) => list.reduce((sum, next) => sum + next, 0)
 export const max = (list: number[]) => list.reduce((best, next) => best > next ? best : next)
 export const sort = (list: number[], ascending = true) => list.sort((a, b) => ascending ? b - a : a - b)
 export const intersection = <T>(a: Set<T>, b: Set<T>) => new Set(Array.from(a).filter(element => b.has(element)))
-export const split = <T>(array: T[], separator: T) => {
+export const split = <T>(array: T[], separator: T | ((item: T) => boolean), includeSeparator = false) => {
   const result: T[][] = [[]]
-  array.forEach(value => value == separator ? result.push([]): result.at(-1)!.push(value))
+  const separatorFunction = typeof separator == 'function' ?
+    separator as (item: T) => boolean :
+    (item: T) => item == separator
+  array.forEach(value => {
+    const isSeparator = separatorFunction(value)
+    if (isSeparator && result.at(-1)!.length != 0) result.push([])
+    if (!isSeparator || includeSeparator) result.at(-1)!.push(value)
+  })
   return result
 }
